45850ecbfb19d6a5914b4f5b2167072bd64cf9a0
bsh <bai.shan@immomo.com>
2017-11-09 15:55:45 +0800

    update

--- app/app.config.js
+++ app/app.config.js
@@ -4,15 +4,15 @@ angular.
     module('coffeesApp').
     config(['$locationProvider' ,'$routeProvider',
         function config($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
+            $locationProvider.hashPrefix('!');
 
-        $routeProvider.
-            when('/coffees', {
-                template: '<coffee-list></coffee-list>'
-            }).
-            when('/coffees/:coffeeId', {
-                template: '<coffee-review></coffee-review>'
-            }).
-            otherwise('/coffees');
+            $routeProvider.
+                when('/coffees', {
+                    template: '<coffee-list></coffee-list>'
+                }).
+                when('/coffees/:coffeeId', {
+                    template: '<coffee-review></coffee-review>'
+                }).
+                otherwise('/coffees');
         }
     ]);
